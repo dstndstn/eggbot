cb34da7b6ab1563878d0ba2a81a51e46497aa76d
Dustin Lang <dstndstn@gmail.com>
2017-04-17 11:48:58 -0400

    plot out a strip chart

--- eggbot.ino
+++ eggbot.ino
@@ -200,18 +200,61 @@ public:
         theta += dtheta;
         go_to(theta);
     }
+
+    float waveform(float phase) {
+      // cos-like.
+      phase = fmod(phase, 2.*M_PI);
+      if (phase < 0)
+        phase += 2.*M_PI;
+      // remap to 0 to 4.
+      phase *= 4. / (2.*M_PI);
+
+      // triangle wave
+      if (phase < 2)
+        return 1 - phase;
+      return (phase-2) - 1;
+
+      /*
+      float xx[] = { 0., 0.4, 0.8,  1.2,  1.6,  2.0 };
+      float yy[] = { 1., 0.6, 0.4, -0.4, -0.6, -1.0 };
+
+      for (int i=0; i<(sizeof(xx)/sizeof(float))-1; i++) {
+        if ((phase >= xx[i]) && (phase <= xx[i+1])) {
+          float dd = (yy[i+1] - yy[i]) / (xx[i+1] - xx[i]);
+          return yy[i] + (phase - xx[i]) * dd;
+        }
+      }
+
+      // triangle wave
+      if (phase < 2) {
+        if (phase < 0.5) {
+          return (1 - phase*1.5);
+        }
+        if (phase < 1.5) {
+          return (1 - (0.5*1.5) - (phase - 0.5)*0.5);
+        }
+        return (-0.25 - (phase - 1.5)*1.5);
+        //return 1 - phase;
+      }
+      return (phase-2) - 1;
+      */
+    }
+    
     void go_to(float t) {
         theta = t;
-        float s = sin(theta);
-        float c = cos(theta);
+        //float s = sin(theta);
+        //float c = cos(theta);
+
+        float c = waveform(theta);
+        float s = waveform(theta + 1.5*M_PI);
 
         //s = (s > 0 ? s*s : -s*s);
         //c = (c > 0 ? c*c : -c*c);
         //s = (s > 0 ? sqrt(s) : -sqrt(-s));
         //c = (c > 0 ? sqrt(c) : -sqrt(-c));
 
-        int ic = (int)(254 * c);
-        int is = (int)(254 * s);
+        int ic = (int)(255 * c);
+        int is = (int)(255 * s);
         int sgn;
         sgn = isign(ic);
         // If we're flipping signs (powering up the other winding),
@@ -274,14 +317,21 @@ MotorB mb;
 
 int ma = 0;
 
-float theta = 0.;
+//float theta = M_PI;
 int d = 1;
-float dtheta = -0.01;
+//float dtheta = -0.01;
+
+float theta = 0;
+//float dtheta = 0.01;
+float DT = 2.*M_PI / 256.0;
+float dtheta = DT;
 
 int inited = 0;
 
 float r = 0;
 float dr = 2.*M_PI / 80;
+
+int aa = 0;
 
 void loop() {
 
@@ -292,7 +342,8 @@ void loop() {
         delay(d);
       }
     */
-      for (int j=0; j<10; j++) {
+    /*
+      for (int j=0; j<3; j++) {
         digitalWrite(PEN_DOWN, HIGH);
         delay(50);
         digitalWrite(PEN_DOWN, LOW);
@@ -304,21 +355,120 @@ void loop() {
       digitalWrite(PEN_UP, HIGH);
       delay(5000);
       digitalWrite(PEN_UP, LOW);
-      
+      digitalWrite(PEN_DOWN, HIGH);
+      delay(25);
+      digitalWrite(PEN_DOWN, LOW);
+      */
+
+     /*
+      digitalWrite(PEN_UP, HIGH);
+      delay(100);
+      for (int i=0; i<5000; i++) {
+        mb.step(-dtheta);
+        delay(d);
+      }
+      for (int i=0; i<1500; i++) {
+        mb.step(dtheta);
+        delay(d);
+      }
+      digitalWrite(PEN_UP, LOW);
+      delay(100);
+      */
+
+      mb.theta = 0.;
+      mb.go_to(0.);
+      mb.theta = 0.;
+      delay(1000);
+
+      for (int i=0; i<800; i++) {
+        ma++;
+        motor_A_to(ma);
+        delay(20 * d);
+      }
+
+
       inited = 1;
   }
 
+  if ((dtheta > 0) && (mb.theta >= 2.*M_PI)) {
+      //dtheta *= -1;
 
-float dr = 2.*M_PI / 30;
+      mb.go_to(2.*M_PI);
+      delay(100);
+      
+      for (int i=0; i<800; i++) {
+        ma++;
+        motor_A_to(ma);
+        delay(20 * d);
+      }
+      digitalWrite(PEN_UP, HIGH);    
+      for (;;) {
+         delay(1000);
+      }
+      
+      dtheta = -0.1;
 
+      digitalWrite(PEN_UP, HIGH);
+      delay(500);
+
+      while (mb.theta > 0) {
+          mb.step(dtheta);
+          delay(10*d);
+      }
+      aa = 0;
+
+      digitalWrite(PEN_UP, LOW);
+      delay(500);
+
+  //}
+  //if ((dtheta < 0) && (mb.theta < 0)) { //-3 * M_PI)) {
+      dtheta = DT;
+      mb.theta = 0.0 - dtheta;
+      mb.go_to(0.0);
+
+  //    digitalWrite(PEN_UP, LOW);
+  //    delay(500);
+
+  }
+
+
+  //if ((aa > 0) && (aa % 8 == 0)) {
+      digitalWrite(PEN_UP, HIGH);
+      delay(100);
+      for (int i=0; i<5; i++) {
+        ma++;
+        motor_A_to(ma);
+        delay(20 * d);
+      }
+      digitalWrite(PEN_UP, LOW);
+      delay(100);
+
+      /*
+      for (int i=0; i<64; i++) {
+        mb.step(dtheta);
+        delay(5*d);
+      }
+      for (int i=0; i<64; i++) {
+        mb.step(-dtheta);
+        delay(5*d);
+      }
+      */
+  //}
+  aa++;
+
+  mb.step(dtheta);
+  delay(10*d);
+
+/*
+    float dr = 2.*M_PI / 200;
     for (int i=0; i<10; i++) {
       float s = sin(r);
       r += dr/10.0;
-      //float t = s * 0.7 * M_PI;
       float t = s * M_PI;
       mb.go_to(t);
       delay(20*d);
     }
+*/
     
     ma++;
     motor_A_to(ma);
